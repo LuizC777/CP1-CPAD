9c8974a776e04834dc89ab7cd9d5836bdcec499d
Luiz Claro <luiz.claro.lima@gmail.com>
2026-03-21 20:49:20 -0300

    Estilização do tabs e do header, inicio do desenvolvimento da pagina home

--- app/HomeScreen.js
+++ app/HomeScreen.js
@@ -4,38 +4,50 @@ export default function Home() {
   const router = useRouter();
   return (
     <View style={styles.container}>
-      <View style={styles.foto}>
-        <Text style={styles.alt}>L</Text>
+      <View style={styles.GroupSession}>
+        <View style={styles.GroupLeft}>
+          <Text style={styles.GroupTitle}>Titulo</Text>
+        </View>
+        <View style={styles.GroupRight}>
+          <Text style={styles.GroupTitle}>FOTO</Text>
+        </View>
       </View>
 
-      <Text style={styles.nome}>Luiz Claro Lima</Text>
-      <TouchableOpacity
-        style={styles.botao}
-        onPress={() => router.push("/perfil")}
-      >
-        <Text style={styles.botaoTexto}>Ver meu perfil</Text>
-      </TouchableOpacity>
+      <View style={styles.GroupSession}>
+        <Text style={styles.GroupTitle}>TESTE</Text>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
-  alt: { fontSize: 70, color: "white" },
-  foto: {
-    height: 150,
-    width: 150,
-    margin: 50,
-    backgroundColor: "#44f",
-    borderRadius: 999,
-    alignItems: "center",
-    justifyContent: "center",
-  },
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#f5f5f5",
+    backgroundColor: "#333",
   },
-  nome: { fontSize: 35, fontWeight: "bold", marginBottom: 24 },
-  botao: { backgroundColor: "#222", padding: 16, borderRadius: 12 },
-  botaoTexto: { color: "#fff", fontSize: 16, fontWeight: "600" },
+  GroupSession: {
+    padding: 5,
+    flex: 1,
+    flexDirection: "row",
+    height: 250,
+    width: 350,
+    backgroundColor: "#E1306C",
+    borderRadius: 30,
+    marginBottom: 30,
+  },
+  GroupTitle: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  GroupLeft: {
+    backgroundColor: "red",
+    flex: 1,
+    padding: 15,
+  },
+  GroupRight: {
+    backgroundColor: "green",
+    flex: 1,
+  },
 });
--- app/_layout.js
+++ app/_layout.js
@@ -1,37 +1,91 @@
 import { Tabs } from "expo-router";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { MaterialIcons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 
 export default function Layout() {
+  const router = useRouter();
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "#000" }}>
+    <Tabs
+      screenOptions={{
+        headerLeft: () => (
+          <TouchableOpacity
+            onPress={() => router.push("/ScreenThree")}
+            style={styles.headerButton}
+          >
+            <Ionicons
+              name="person-circle-outline"
+              size={35}
+              color={"#E1306C"}
+            />
+            <View>
+              <Text style={styles.headerText}>LUIZ CLARO LIMA</Text>
+              <Text style={styles.headerText}>2CCPO</Text>
+            </View>
+          </TouchableOpacity>
+        ),
+        headerRight: () => <Text style={styles.fiap}>FIAP</Text>,
+        tabBarStyle: {
+          backgroundColor: "#111",
+          borderTopWidth: 0,
+        },
+        headerStyle: {
+          backgroundColor: "#111",
+        },
+        headerTintColor: "#E1306C",
+        headerTitleAlign: "center",
+        headerShadowVisible: false,
+        tabBarActiveTintColor: "#E1306C",
+      }}
+    >
       <Tabs.Screen
         name="HomeScreen"
         options={{
-          title: "TELA 1",
+          tabBarShowLabel: false,
+          headerTitle: "",
           tabBarIcon: ({ color }) => (
-            <Ionicons name="bug-outline" size={24} color={color} />
+            <Ionicons name="home" size={22} color={color} />
           ),
         }}
       />
       <Tabs.Screen
         name="ScreenTwo"
         options={{
-          title: "TELA 2",
+          tabBarShowLabel: false,
+          headerTitle: "",
           tabBarIcon: ({ color }) => (
-            <Ionicons name="bug-outline" size={24} color={color} />
+            <Ionicons name="bug-outline" size={22} color={color} />
           ),
         }}
       />
       <Tabs.Screen
         name="ScreenThree"
         options={{
-          title: "TELA 3",
+          tabBarShowLabel: false,
+          headerTitle: "",
           tabBarIcon: ({ color }) => (
-            <Ionicons name="bug-outline" size={24} color={color} />
+            <Ionicons name="person-circle" size={22} color={color} />
           ),
         }}
       />
     </Tabs>
   );
 }
+const styles = StyleSheet.create({
+  headerButton: {
+    marginLeft: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  headerText: {
+    color: "#E1306C",
+    marginLeft: 5,
+    fontSize: 15,
+  },
+  fiap: {
+    color: "#E1306C",
+    fontSize: 35,
+    marginRight: 20,
+    marginBottom: 2,
+  },
+});
