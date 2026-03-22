4b517c2e7331a362b10dcdc67329d4d5aa455489
Luiz Claro <luiz.claro.lima@gmail.com>
2026-03-22 19:06:55 -0300

    finalização do homeScreen

--- app/HomeScreen.js
+++ app/HomeScreen.js
@@ -4,13 +4,19 @@ import {
   TouchableOpacity,
   StyleSheet,
   ScrollView,
+  Image,
 } from "react-native";
+import { useState, useEffect } from "react";
 import { useRouter } from "expo-router";
 export default function Home() {
+  const [active1, setActive1] = useState(false);
+  const [active2, setActive2] = useState(false);
+  const [active3, setActive3] = useState(false);
+
   const router = useRouter();
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.GroupSession1}>
+      <View style={[styles.GroupSession, styles.sessionBg1]}>
         <View style={styles.GroupLeft}>
           <View>
             <Text style={styles.GroupTitle}>Front-end</Text>
@@ -19,19 +25,30 @@ export default function Home() {
           <View>
             <Text style={styles.GroupDesc}>30 de Março</Text>
             <Text style={styles.GroupDesc}>Laboratório 707</Text>
-            <TouchableOpacity style={styles.Button}>
-              <Text style={styles.ButtonText}>Entrar</Text>
+
+            <TouchableOpacity
+              onPress={() => setActive1(!active1)}
+              style={[styles.Button, active1 ? styles.active : styles.inactive]}
+            >
+              <Text
+                style={[
+                  styles.ButtonText,
+                  active1 ? styles.ButtonTextActive : styles.ButtonTextInactive,
+                ]}
+              >
+                {active1 ? "Entrou" : "Entrar"}
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
         <View style={styles.GroupRight}>
           <View style={styles.ImgContainer}>
-            <Text></Text>
+            <Image source={require("../assets/html.jpg")} style={styles.Img} />
           </View>
         </View>
       </View>
 
-      <View style={styles.GroupSession2}>
+      <View style={[styles.GroupSession, styles.sessionBg2]}>
         <View style={styles.GroupLeft}>
           <View>
             <Text style={styles.GroupTitle}>Pandas</Text>
@@ -40,19 +57,32 @@ export default function Home() {
           <View>
             <Text style={styles.GroupDesc}>05 de Abril</Text>
             <Text style={styles.GroupDesc}>Laboratório 302</Text>
-            <TouchableOpacity style={styles.Button}>
-              <Text style={styles.ButtonText}>Entrar</Text>
+            <TouchableOpacity
+              onPress={() => setActive2(!active2)}
+              style={[styles.Button, active2 ? styles.active : styles.inactive]}
+            >
+              <Text
+                style={[
+                  styles.ButtonText,
+                  active2 ? styles.ButtonTextActive : styles.ButtonTextInactive,
+                ]}
+              >
+                {active2 ? "Entrou" : "Entrar"}
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
         <View style={styles.GroupRight}>
           <View style={styles.ImgContainer}>
-            <Text></Text>
+            <Image
+              source={require("../assets/grafico.png")}
+              style={styles.Img}
+            />
           </View>
         </View>
       </View>
 
-      <View style={styles.GroupSession3}>
+      <View style={[styles.GroupSession, styles.sessionBg3]}>
         <View style={styles.GroupLeft}>
           <View>
             <Text style={styles.GroupTitle}>JavaScript</Text>
@@ -61,14 +91,27 @@ export default function Home() {
           <View>
             <Text style={styles.GroupDesc}>25 de Março</Text>
             <Text style={styles.GroupDesc}>Laboratório 404</Text>
-            <TouchableOpacity style={styles.Button}>
-              <Text style={styles.ButtonText}>Entrar</Text>
+            <TouchableOpacity
+              onPress={() => setActive3(!active3)}
+              style={[styles.Button, active3 ? styles.active : styles.inactive]}
+            >
+              <Text
+                style={[
+                  styles.ButtonText,
+                  active3 ? styles.ButtonTextActive : styles.ButtonTextInactive,
+                ]}
+              >
+                {active3 ? "Entrou" : "Entrar"}
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
         <View style={styles.GroupRight}>
           <View style={styles.ImgContainer}>
-            <Text></Text>
+            <Image
+              source={require("../assets/codigo.webp")}
+              style={styles.Img}
+            />
           </View>
         </View>
       </View>
@@ -81,15 +124,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#333",
   },
-  ImgContainer: {
-    width: 140,
-    height: 200,
-    backgroundColor: "#fff",
-    borderRadius: 30,
-    opacity: 0.8,
-  },
+
   Button: {
-    backgroundColor: "#fff",
     width: 100,
     height: 30,
     alignItems: "center",
@@ -97,38 +133,29 @@ const styles = StyleSheet.create({
     marginTop: 40,
     borderRadius: 20,
   },
+  active: { backgroundColor: "#000" },
+  inactive: { backgroundColor: "#fff" },
   ButtonText: {
     fontSize: 17,
     fontWeight: "bold",
   },
-  GroupSession1: {
+  ButtonTextActive: { color: "#fff" },
+  ButtonTextInactive: { color: "#000" },
+
+  GroupSession: {
     padding: 5,
     flexDirection: "row",
     height: 280,
     width: 350,
     backgroundColor: "#447",
     borderRadius: 30,
-    marginTop: 25,
+    marginTop: 13,
+    marginBottom: 13,
   },
-  GroupSession2: {
-    padding: 5,
-    flexDirection: "row",
-    height: 280,
-    width: 350,
-    backgroundColor: "#464",
-    borderRadius: 30,
-    marginTop: 25,
-  },
-  GroupSession3: {
-    padding: 5,
-    flexDirection: "row",
-    height: 280,
-    width: 350,
-    backgroundColor: "#ffae00",
-    borderRadius: 30,
-    marginTop: 25,
-    marginBottom: 25,
-  },
+  sessionBg1: { backgroundColor: "#447" },
+  sessionBg2: { backgroundColor: "#464" },
+  sessionBg3: { backgroundColor: "#ffae00" },
+
   GroupTitle: {
     color: "#fff",
     fontWeight: "bold",
@@ -139,15 +166,30 @@ const styles = StyleSheet.create({
     fontSize: 16,
     opacity: 0.7,
   },
+
   GroupLeft: {
-    flex: 1,
+    width: "50%",
     padding: 15,
     justifyContent: "space-between",
   },
   GroupRight: {
-    flex: 1,
+    width: "50%",
     padding: 15,
     alignItems: "center",
     justifyContent: "center",
+  },
+  ImgContainer: {
+    width: 140,
+    height: 200,
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    opacity: 0.8,
+    overflow: "hidden",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  Img: {
+    width: 140,
+    height: 200,
   },
 });
