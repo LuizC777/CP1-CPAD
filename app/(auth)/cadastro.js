874713e693269bca9327fabfe5a6a7a8e5bd7212
Gabriel Nacarelli <gabriel.nacarelli@gmail.com>
2026-05-03 18:14:50 -0300

    feat: atualizando o cadastro

--- app/(auth)/cadastro.js
+++ app/(auth)/cadastro.js
@@ -1,4 +1,13 @@
-import { View, TextInput, Button, Alert } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from "react-native";
 import { useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useState } from "react";
@@ -10,44 +19,270 @@ export default function Register() {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [confirmar, setConfirmar] = useState("");
+  const [linkedin, setLinkedin] = useState("");
+  const [github, setGithub] = useState("");
+  const [tel, setTel] = useState("");
+
+  const [erroEmail, setErroEmail] = useState("");
+  const [erroSenha, setErroSenha] = useState("");
+  const [erroConfirmar, setErroConfirmar] = useState("");
+  const [erroNome, setErroNome] = useState("");
+  const [erroLinkedin, setErroLinkedin] = useState("");
+  const [erroGithub, setErroGithub] = useState("");
+  const [erroTel, setErroTel] = useState("");
+
+  const validarEmail = (email) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  const validarSenha = (senha) => {
+    return senha.length >= 6;
+  };
+
+  const validarLinkedin = (url) => {
+    return url.includes("linkedin.com");
+  };
+
+  const validarGithub = (url) => {
+    return url.includes("github.com");
+  };
+
+  const validarTel = (tel) => {
+    return /^\d{10,11}$/.test(tel);
+  };
 
   const handleRegister = async () => {
-    if (!nome || !email || !senha) {
-      return Alert.alert("Preencha tudo");
+    let valido = true;
+
+    setErroNome("");
+    setErroEmail("");
+    setErroSenha("");
+    setErroConfirmar("");
+    setErroLinkedin("");
+    setErroGithub("");
+    setErroTel("");
+
+    if (!nome) {
+      setErroNome("Digite seu nome");
+      valido = false;
     }
 
-    if (senha.length < 6) {
-      return Alert.alert("Senha muito curta");
+    if (!validarEmail(email)) {
+      setErroEmail("Email inválido");
+      valido = false;
+    }
+
+    if (!validarSenha(senha)) {
+      setErroSenha("A senha deve ter pelo menos 6 caracteres");
+      valido = false;
     }
 
     if (senha !== confirmar) {
-      return Alert.alert("Senhas diferentes");
+      setErroConfirmar("As senhas não coincidem");
+      valido = false;
     }
+
+    if (linkedin && !validarLinkedin(linkedin)) {
+      setErroLinkedin("Link do LinkedIn inválido");
+      valido = false;
+    }
+
+    if (github && !validarGithub(github)) {
+      setErroGithub("Link do GitHub inválido");
+      valido = false;
+    }
+
+    if (tel && !validarTel(tel)) {
+      setErroTel("Telefone deve ter 10 ou 11 números");
+      valido = false;
+    }
+
+    if (!valido) return;
 
     const usuario = { nome, email, senha };
 
     await AsyncStorage.setItem("usuario", JSON.stringify(usuario));
 
-    Alert.alert("Cadastro feito!");
     router.replace("/login");
   };
 
   return (
-    <View>
-      <TextInput placeholder="Nome" onChangeText={setNome} />
-      <TextInput placeholder="Email" onChangeText={setEmail} />
-      <TextInput placeholder="Senha" secureTextEntry onChangeText={setSenha} />
-      <TextInput
-        placeholder="Confirmar senha"
-        secureTextEntry
-        onChangeText={setConfirmar}
-      />
+    <KeyboardAvoidingView
+      style={{ flex: 1, backgroundColor: "#222" }}
+      behavior="padding"
+    >
+      <ScrollView
+        contentContainerStyle={styles.container}
+        keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.Header}>
+          <Text style={styles.fiap}>FIAP</Text>
+          <Text style={styles.conecta}>conecta</Text>
+        </View>
 
-      <Button title="Cadastrar" onPress={handleRegister} />
-      <Button
-        title="Voltar para login"
-        onPress={() => router.replace("/login")}
-      />
-    </View>
+        <View style={styles.body}>
+          <Text style={styles.sectionTitle}>Cadastro</Text>
+          <TextInput
+            placeholder="Nome"
+            placeholderTextColor="#ddd"
+            onChangeText={setNome}
+            style={styles.input}
+          />
+          {erroNome ? <Text style={styles.erro}>{erroNome}</Text> : null}
+
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor="#ddd"
+            onChangeText={setEmail}
+            style={styles.input}
+          />
+          {erroEmail ? <Text style={styles.erro}>{erroEmail}</Text> : null}
+
+          <TextInput
+            placeholder="Senha"
+            placeholderTextColor="#ddd"
+            secureTextEntry
+            onChangeText={setSenha}
+            style={styles.input}
+          />
+          {erroSenha ? <Text style={styles.erro}>{erroSenha}</Text> : null}
+
+          <TextInput
+            placeholder="Confirmar senha"
+            placeholderTextColor="#ddd"
+            secureTextEntry
+            onChangeText={setConfirmar}
+            style={styles.input}
+          />
+          {erroConfirmar ? (
+            <Text style={styles.erro}>{erroConfirmar}</Text>
+          ) : null}
+          <Text style={styles.sectionTitle}>Informações adicionais</Text>
+
+          <TextInput
+            placeholder="LinkedIn"
+            placeholderTextColor="#ddd"
+            onChangeText={setLinkedin}
+            style={styles.input}
+          />
+          {erroLinkedin ? (
+            <Text style={styles.erro}>{erroLinkedin}</Text>
+          ) : null}
+
+          <TextInput
+            placeholder="GitHub"
+            placeholderTextColor="#ddd"
+            onChangeText={setGithub}
+            style={styles.input}
+          />
+          {erroGithub ? <Text style={styles.erro}>{erroGithub}</Text> : null}
+
+          <TextInput
+            placeholder="Telefone"
+            placeholderTextColor="#ddd"
+            keyboardType="numeric"
+            onChangeText={setTel}
+            style={styles.input}
+          />
+          {erroTel ? <Text style={styles.erro}>{erroTel}</Text> : null}
+
+          <TouchableOpacity style={styles.botao} onPress={handleRegister}>
+            <Text style={styles.textoBotao}>Cadastrar</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => router.replace("/login")}>
+            <Text style={styles.link}>
+              Já tem uma conta? <Text style={styles.linkDestaque}>Entrar</Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#222",
+  },
+
+  Header: {
+    height: 100,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    marginBottom: 40,
+  },
+
+  fiap: {
+    color: "#E1306C",
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+
+  conecta: {
+    color: "#E1306C",
+    fontSize: 25,
+    marginLeft: 10,
+    fontStyle: "italic",
+  },
+
+  body: {
+    flex: 1,
+    justifyContent: "flex-start",
+    backgroundColor: "#222",
+  },
+
+  input: {
+    backgroundColor: "#777",
+    marginBottom: 20,
+    marginHorizontal: 10,
+    padding: 10,
+    borderRadius: 99,
+    color: "#fff",
+  },
+
+  botao: {
+    backgroundColor: "#E1306C",
+    padding: 12,
+    borderRadius: 30,
+    alignItems: "center",
+    marginHorizontal: 10,
+    marginTop: 10,
+  },
+
+  textoBotao: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+
+  link: {
+    color: "#ccc",
+    textAlign: "center",
+    marginTop: 20,
+  },
+
+  linkDestaque: {
+    color: "#E1306C",
+    fontWeight: "bold",
+  },
+
+  erro: {
+    color: "#ff4d4d",
+    marginLeft: 15,
+    marginTop: -15,
+    marginBottom: 10,
+    fontSize: 12,
+  },
+  sectionTitle: {
+    color: "#fff",
+    fontSize: 18,
+    marginLeft: 10,
+    marginTop: 10,
+    marginBottom: 20,
+    fontWeight: "bold",
+  },
+});
